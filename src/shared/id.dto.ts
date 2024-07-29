86fe37e55ab3d1eb8eb02aa85a6b73974ebbd344
Đậu Khoa <khoakomlem@gmail.com>
2024-07-29 18:55:09 +0700

    fix id dto

--- src/shared/id.dto.ts
+++ src/shared/id.dto.ts
@@ -2,8 +2,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { StringToBuffer } from 'src/utils/StringToBuffer';
 
 export class IdDto {
-  @IsString()
-  @IsNotEmpty()
   @StringToBuffer()
+  @IsNotEmpty()
   id: Buffer;
 }
