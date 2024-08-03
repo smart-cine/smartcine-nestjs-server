41ae37a12391a1658497efec5743e12dce919657
Đậu Khoa <khoakomlem@gmail.com>
2024-08-04 02:43:30 +0700

    remove nullable (useless to IsOptional)

--- src/utils/StringToBuffer.ts
+++ src/utils/StringToBuffer.ts
@@ -1,12 +1,12 @@
 import { Transform } from 'class-transformer';
 import { uuidToBinary } from './uuid';
 
-export function StringToBuffer({ nullable = false } = {}) {
+export function StringToBuffer() {
   return Transform(({ value }) => {
     if (typeof value === 'string') {
       return uuidToBinary(value);
     }
 
-    if (!nullable) throw new Error('Value must be a string!');
+    throw new Error('Value must be a string!');
   });
 }
