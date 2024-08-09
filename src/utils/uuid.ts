d9d597bfa5050132f5d9f1b5255a0db089e8aeaf
Đậu Khoa <khoakomlem@gmail.com>
2024-08-10 03:19:03 +0700

    generic binaryToUuid

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,6 +1,12 @@
-export function binaryToUuid(buffer: Buffer) {
+export function binaryToUuid<
+  T extends Buffer | undefined | null,
+  ResponseT extends T extends Buffer ? string : null,
+>(buffer: T): ResponseT {
+  if (!buffer) {
+    return null as ResponseT;
+  }
   const hexString = buffer.toString('hex');
-  return `${hexString.slice(0, 8)}-${hexString.slice(8, 12)}-${hexString.slice(12, 16)}-${hexString.slice(16, 20)}-${hexString.slice(20)}`;
+  return `${hexString.slice(0, 8)}-${hexString.slice(8, 12)}-${hexString.slice(12, 16)}-${hexString.slice(16, 20)}-${hexString.slice(20)}` as ResponseT;
 }
 
 export function uuidToBinary(uuid: string) {
