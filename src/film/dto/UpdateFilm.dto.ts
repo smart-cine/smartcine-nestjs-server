1c9d2bf7f98be1819f9f9a06ea8b918702287f93
Đậu Khoa <khoakomlem@gmail.com>
2024-08-10 03:32:58 +0700

    refactor update film

--- src/film/dto/UpdateFilm.dto.ts
+++ src/film/dto/UpdateFilm.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import {
   IsString,
   IsNotEmpty,
@@ -7,54 +8,20 @@ import {
   IsOptional,
 } from 'class-validator';
 import { StringToBuffer } from 'src/utils/StringToBuffer';
+import { CreateFilmDto } from './CreateFilm.dto';
 
-export class UpdateFilmDto {
-  @StringToBuffer()
-  @IsNotEmpty()
-  @IsString()
-  cinema_provider_id: Buffer;
-
-  @IsNotEmpty()
-  @IsString()
-  title: string;
-
-  @IsNotEmpty()
-  @IsString()
-  director: string;
-
-  @IsNotEmpty()
-  @IsString()
-  description: string;
-
-  @IsDate()
-  release_date: Date;
-
-  @IsNotEmpty()
-  @IsString()
-  country: string;
-
-  @IsPositive()
-  @IsNumber()
-  @IsOptional()
-  restrict_age: number;
-
-  @IsPositive()
-  @IsNumber()
-  duration: number;
-
-  @IsNotEmpty()
-  @IsString()
-  picture_url: string;
-
-  @IsNotEmpty()
-  @IsString()
-  background_url: string;
-
-  @IsNotEmpty()
-  @IsString()
-  trailer_url: string;
-
-  @IsNotEmpty()
-  @IsString()
-  language: string;
-}
+export class UpdateFilmDto extends PartialType(
+  PickType(CreateFilmDto, [
+    'title',
+    'director',
+    'description',
+    'release_date',
+    'country',
+    'restrict_age',
+    'duration',
+    'picture_url',
+    'background_url',
+    'trailer_url',
+    'language',
+  ]),
+) {}
