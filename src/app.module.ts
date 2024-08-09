0a9c37d0348954d98e72a044ffd11da566d51a63
Đậu Khoa <khoakomlem@gmail.com>
2024-08-10 03:18:15 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,24 @@ import { AccountModule } from './account/account.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtOptions } from './shared/JwtOptions';
 import { RedisModule } from './redis/redis.module';
-import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { RolesGuard } from './account/guards/roles.guard';
 import { ResponseInterceptor } from './response/Response.intercept';
 import { PickseatModule } from './pickseat/pickseat.module';
 import { PerformModule } from './perform/perform.module';
-import { CinemaLayoutSeatModule } from './cinema-layout-seat/cinema-layout-seat.module';
-import { CinemaLayoutModule } from './cinema-layout/cinema-layout.module';
 import { CinemaRoomModule } from './cinema-room/cinema-room.module';
 import { CinemaModule } from './cinema/cinema.module';
 import { CinemaProviderModule } from './cinema-provider/cinema-provider.module';
+import { CinemaLayoutModule } from './cinema-layout/cinema-layout.module';
+import { CinemaLayoutSeatModule } from './cinema-layout-seat/cinema-layout-seat.module';
+import { CinemaLayoutGroupModule } from './cinema-layout-group/cinema-layout-group.module';
+import { TagModule } from './tag/tag.module';
+import { ItemModule } from './item/item.module';
+import { CommentModule } from './comment/comment.module';
+import { RatingModule } from './rating/rating.module';
+import { JwtParserGuard } from './account/guards/jwt-parser.guard';
+import { PermissionsGuard } from './account/guards/permissions.guard';
+import { HttpExceptionFilter } from './response/filter/http-exception.filter';
 
 @Module({
   imports: [
@@ -37,15 +45,32 @@ import { CinemaProviderModule } from './cinema-provider/cinema-provider.module';
     CinemaRoomModule,
     CinemaLayoutModule,
     CinemaLayoutSeatModule,
+    CinemaLayoutGroupModule,
+    TagModule,
+    ItemModule,
+    CommentModule,
+    RatingModule,
   ],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtParserGuard,
+    },
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
     {
+      provide: APP_GUARD,
+      useClass: PermissionsGuard,
+    },
+    {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
     {
       provide: APP_PIPE,
