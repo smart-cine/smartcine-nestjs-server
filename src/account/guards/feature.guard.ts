22dbc23b56fecc15405efa9f3810489b59a2d10f
Đậu Khoa <khoakomlem@gmail.com>
2024-08-27 23:42:56 +0700

    add feature guard

---
+++ src/account/guards/feature.guard.ts
@@ -0,0 +1,60 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Feature } from '../decorators/feature.decorator';
+import { TCustomRequest } from '../types/TCustomRequest';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class FeatureGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private prismaService: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const feature =
+      this.reflector.get(Feature, context.getHandler()) ??
+      this.reflector.get(Feature, context.getClass());
+
+    if (!feature) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<TCustomRequest>();
+    if (!request.account) {
+      throw new UnauthorizedException('Permission denied');
+    }
+
+    try {
+      const ownership = await this.prismaService.ownership.findUniqueOrThrow({
+        where: {
+          owner_id: request.account.id,
+        },
+        select: {
+          item_id: true,
+          role: true,
+        },
+      });
+
+      console.log(ownership, feature);
+      await this.prismaService.roleToFeature.findUniqueOrThrow({
+        where: {
+          role_feature: {
+            role: ownership.role,
+            feature: feature,
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException('Permission denied');
+    }
+
+    return true;
+  }
+}
